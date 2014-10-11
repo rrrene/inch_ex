ed06a6accac64ddfb001c84395aea2c8c6681449
René Föhring <rf@bamaru.de>
2014-10-11 11:44:40 +0200

    Bump version to 0.2.1

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 0.2.1
+
+- Added more specific console output in case `mix inch.report` fails
+
+## 0.2.0
+
+- Initial release
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule InchEx.Mixfile do
   def project do
     [
       app: :inch_ex,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.0",
       description: "Elixir wrapper for Inch",
       package: [
