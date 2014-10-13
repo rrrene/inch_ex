0fda3c0ec8e659bce8feb4ef2d9ec3fab3e924d0
René Föhring <rf@bamaru.de>
2014-10-13 23:40:44 +0200

    Bump version to 0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule InchEx.Mixfile do
   def project do
     [
       app: :inch_ex,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.0",
       description: "Elixir wrapper for Inch",
       package: [
