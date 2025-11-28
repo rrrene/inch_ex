3a07bf3805b490a91725682b4d89a11b3d1b7d52
René Föhring <rf@bamaru.de>
2025-11-28 09:29:42 +0100

    Fix integration test for 1.18.4

--- test_fixtures/inch_test/config/config.exs
+++ test_fixtures/inch_test/config/config.exs
@@ -40,4 +40,4 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
+# import_config "#{config_env()}.exs"
