b29a10a1f0748345ec79e490ebc036b3e171c35a
René Föhring <rf@bamaru.de>
2025-12-01 11:19:36 +0100

    Bump version to 2.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule InchEx.Mixfile do
   def project do
     [
       app: :inch_ex,
-      version: "2.1.0-rc.1",
+      version: "2.1.0",
       elixir: ">= 1.15.0",
       description: "Provides a Mix task that gives you hints where to improve your inline docs",
       source_url: "https://github.com/rrrene/inch_ex",
