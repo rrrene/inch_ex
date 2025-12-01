315e8656295f1a018d0f82883b0bc556fb05b5e0
René Föhring <rf@bamaru.de>
2025-12-01 11:15:38 +0100

    Clean up unused files

--- test/inch_ex_test.exs
+++ test/inch_ex_test.exs
@@ -1,26 +1,6 @@
 defmodule InchExTest do
   use ExUnit.Case
 
-  @tag :skip
-  test "json read" do
-    input =
-      "inch_test_input.json"
-      |> File.read!()
-      |> Jason.decode!()
-      |> Map.get("objects")
-
-    expected =
-      "inch_test_result.json"
-      |> File.read!()
-      |> Jason.decode!()
-      |> Map.get("objects")
-
-    results = InchEx.CodeObject.eval(input)
-
-    assert List.first(expected) == List.first(results)
-    assert expected == results
-  end
-
   @tag :test_fixtures
   test "inch_test does work with inch_ex" do
     "inch_test"
